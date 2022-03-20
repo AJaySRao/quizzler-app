74aece4282d98162f70bdfc500df868861417f20
changeAJay <ajajay008@gmail.com>
2022-03-20 19:38:24 +0530

    Bug fixes

--- ui.py
+++ ui.py
@@ -24,12 +24,12 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.true_img = PhotoImage(file="images/true.png")
-        self.button = Button(image=self.true_img, highlightthickness=0, command=self.is_true)
-        self.button.grid(row=2, column=0)
+        self.t_button = Button(image=self.true_img, highlightthickness=0, command=self.is_true)
+        self.t_button.grid(row=2, column=0)
 
         self.false_img = PhotoImage(file="images/false.png")
-        self.button = Button(image=self.false_img, highlightthickness=0, command=self.is_false)
-        self.button.grid(row=2, column=1)
+        self.f_button = Button(image=self.false_img, highlightthickness=0, command=self.is_false)
+        self.f_button.grid(row=2, column=1)
 
         self.get_next_question()
 
@@ -37,9 +37,14 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        self.label.config(text=f"SCORE: {self.quiz.score}")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.label.config(text=f"SCORE: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You completed the Quiz!")
+            self.t_button.config(state="disabled")
+            self.f_button.config(state="disabled")
 
     def is_true(self):
         right = self.quiz.check_answer("True")
