ab400cc385f73f444e0fb9cc3fc2fa3d408907f3
changeAJay <ajajay008@gmail.com>
2022-03-20 18:51:23 +0530

    Updating True/False

--- ui.py
+++ ui.py
@@ -7,12 +7,13 @@ THEME_COLOR = "#375362"
 class QuizInterface:
     def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
+        self.score = self.quiz.score
 
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.label = Label(text='SCORE: 0', bg=THEME_COLOR, fg="white")
+        self.label = Label(text=f"SCORE:{self.score}", bg=THEME_COLOR, fg="white")
         self.label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
@@ -24,11 +25,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.true_img = PhotoImage(file="images/true.png")
-        self.button = Button(image=self.true_img, highlightthickness=0, command=self.get_next_question)
+        self.button = Button(image=self.true_img, highlightthickness=0, command=self.is_true)
         self.button.grid(row=2, column=0)
 
         self.false_img = PhotoImage(file="images/false.png")
-        self.button = Button(image=self.false_img, highlightthickness=0, command=self.get_next_question)
+        self.button = Button(image=self.false_img, highlightthickness=0, command=self.is_false)
         self.button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -38,3 +39,12 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def is_true(self):
+        self.quiz.check_answer("True")
+        self.get_next_question()
+
+    def is_false(self):
+        self.quiz.check_answer("False")
+        self.get_next_question()
+
